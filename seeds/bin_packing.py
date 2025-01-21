4db5b5c83674c828081ad916b4d7a9e83a3c17ad
Bill Huang <bill.huang2001@gmail.com>
2025-01-21 14:34:33 +0800

    Update docstring

--- seeds/bin_packing.py
+++ seeds/bin_packing.py
@@ -1,15 +1,14 @@
 def bin_packing(items: list[float]) -> list[int]:
-    """Given a list of items, pack them into bins with capacity 1.0, minimizing the number of bins used.
-    The function should returns a packing plan,
-    where each item is assigned to a bin encoded as an integer from 0 to n-1,
-    and n is the total number of bins used.
-    The time complexity of the function should be less than O(n^2).
+    """Pack a list of items into bins with a capacity of 1.0, minimizing the number of bins used.
 
-    For example:
+    The function returns a list where each item is assigned to a bin,
+    represented by an integer from 0 to n-1, where n is the total number of bins used.
+
+    Example:
     >>> items = [0.8, 0.01, 0.2, 0.99]
     >>> bin_packing(items)
     [0, 1, 0, 1]
 
-    The packing plan above means that the first and third items are packed into the first bin, and the second and fourth items are packed into the second bin.
+    In this example, the first and third items are placed in the first bin, and the second and fourth items are placed in the second bin.
     """
     return list(range(len(items)))
