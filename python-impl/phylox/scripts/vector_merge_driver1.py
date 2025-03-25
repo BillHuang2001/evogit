edf5513dc49d0a0ababb8ea91d752089af4f77f1
Bill Huang <bill.huang2001@gmail.com>
2025-03-25 13:36:15 +0800

    Add vector merge drivers

--- python-impl/phylox/scripts/vector_merge_driver1.py
+++ python-impl/phylox/scripts/vector_merge_driver1.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 """Usage: python vector_merge_driver.py <ancestor> <parent1> <parent2>"""
 import sys
 import numpy as np
@@ -20,18 +21,9 @@ delta2 = parent2_vec - ancestor_vec
 # Merge vectors
 ## If there is no conflict, one of the delta is zero, so the other delta is applied
 ## If there is a conflict, it will be handled by a random linear combination in the next step
-child_vec = ancestor_vec + delta1 + delta2
-
-# Resolve conflicts
-## Find the conflicting items
-conflict = (delta1 != 0) & (delta2 != 0) # conflict if both parents try to change the same item
-conflict_vec1 = delta1[conflict]
-conflict_vec2 = delta2[conflict]
-## Random linear combination
 rng = np.random.default_rng()
-factor = rng.uniform(0, 1, conflict_vec1.shape)
-conflict_resolved = conflict_vec1 * factor + conflict_vec2 * (1 - factor)
-## Overwrite the conflicting items
-child_vec[conflict] = conflict_resolved
+factor1 = rng.uniform(0, 1, delta1.shape)
+factor2 = rng.uniform(0, 1, delta2.shape)
+child_vec = ancestor_vec + factor1 * delta1 + factor2 * delta2
 
 np.save(parent1, child_vec)
