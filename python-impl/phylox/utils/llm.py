f0cf9551733cf2e88ec88776f95ae0a409b75e31
Bill Huang <bill.huang2001@gmail.com>
2025-04-14 01:08:15 +0800

    Use azure api

--- python-impl/phylox/utils/llm.py
+++ python-impl/phylox/utils/llm.py
@@ -6,7 +6,9 @@ import logging
 from concurrent.futures import ThreadPoolExecutor
 import time
 from functools import reduce
-
+from azure.ai.inference import ChatCompletionsClient
+from azure.ai.inference.models import SystemMessage, UserMessage
+from azure.core.credentials import AzureKeyCredential
 import httpx
 
 
@@ -90,13 +92,11 @@ class GeminiBackend:
 class TGIBackend:
     def __init__(
         self,
-        base_url: str = "http://127.0.0.1:8080",
-        http_req_params: dict = {},
+        endpoint,
         max_tokens=10000,
         num_workers=4,
         model="",
         api_key="-",
-        extra_args={},
     ) -> None:
         """
         Parameters
@@ -108,66 +108,45 @@ class TGIBackend:
             Common parameters include `timeout`, `proxies`, etc.
         """
         super().__init__()
-        self.url = base_url + "/v1/chat/completions"
-        self.http_req_params = http_req_params
         self.logger = logging.getLogger("phylox")
         self.num_workers = num_workers
         self.num_retry = 100000
         self.usage_history = []
         if num_workers > 1:
             self.pool = ThreadPoolExecutor(max_workers=num_workers)
-        self.extra_args = extra_args
         self.api_key = api_key
         self.model = model
         self.max_tokens = max_tokens
+        if not endpoint.endswith("/"):
+            endpoint = endpoint + "/"
+        self.client = ChatCompletionsClient(
+            endpoint=endpoint + "openai/deployments/" + model,
+            credential=AzureKeyCredential(api_key),
+        )
 
     def _one_restful_request(self, args):
         seed, query = args
-        if len(query) > 1_000_000:
-            query = query[:1_000_000]
-            self.logger.warning("Query too long, truncated to 30,000 characters.")
+        # the context window is 128k, and 4 chars is around 1 token
+        if len(query) > 400_000:
+            query = query[:400_000]
+            self.logger.warning("Query too long, truncated to 400,000 characters.")
 
-        headers = {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer " + self.api_key,
-        }
-        messages = [
-            {
-                "role": "user",
-                "content": query,
-            }
-        ]
-        data = {
-            "messages": messages,
-            "stream": False,
-            "model": self.model,
-            "seed": seed,
-            "max_tokens": self.max_tokens,
-        }
-        data = data | self.extra_args
         for retry in range(self.num_retry):
             try:
-                response = httpx.post(
-                    self.url, headers=headers, json=data, **self.http_req_params
+                response = self.client.complete(
+                    messages=[
+                        UserMessage(content=query),
+                    ],
+                    max_tokens=self.max_tokens,
+                    seed=seed,
+                    model=self.model,
                 )
-                json_response = response.json()
-            except Exception:
-                import traceback
-
-                self.logger.error(f"Failed to query TGI. Sleep. Retry {retry + 1}...")
-                self.logger.error(traceback.format_exc())
-                # network error, sleep and retry
-                time.sleep(30)
-                continue
-
-            try:
-                content = json_response["choices"][0]["message"]["content"]
-                usage = json_response["usage"]
+                content = response.choices[0].message.content
+                usage = response.usage
                 return content, usage
-            except Exception:
-                self.logger.error(f"Failed to parse response: {json_response}")
-                self.logger.error(f"{response.text}")
-                self.logger.error(f"{response.json()}")
+            except Exception as e:
+                self.logger.warning(f"Failed to query TGI: {e}")
+                time.sleep(60)
 
         self.logger.error(f"Failed to query TGI for {self.num_retry} times. Abort!")
         raise Exception("Failed to query TGI")
