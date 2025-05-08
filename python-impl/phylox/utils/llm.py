ca2b632927be7343b7b0736b1ffc4f09b15ce146
Bill Huang <bill.huang2001@gmail.com>
2025-05-08 16:08:52 +0800

    Remove old code

--- python-impl/phylox/utils/llm.py
+++ python-impl/phylox/utils/llm.py
@@ -12,83 +12,6 @@ from azure.core.credentials import AzureKeyCredential
 import httpx
 
 
-class GeminiBackend:
-    def __init__(self, api_key: str, http_req_params: dict = {}, **kwargs) -> None:
-        """
-        Parameters
-        ----------
-        api_key
-            The API key for the Gemini API.
-        http_req_params
-            Additional HTTP request parameters.
-            Common parameters include `timeout`, `proxies`, etc.
-        **kwargs
-            Additional parameters.
-        """
-        super().__init__()
-        self.api_key = api_key
-        self.http_req_params = http_req_params
-        self.url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent"
-        self.logger = logging.getLogger("phylox")
-        self.num_retry = 5
-
-    def restful_request(self, query: str):
-        content_type = "application/json; charset=utf-8"
-        body = {
-            "contents": {"role": "user", "parts": {"text": query}},
-        }
-        try:
-            response = httpx.post(
-                self.url,
-                json=body,
-                headers={
-                    "Content-type": content_type,
-                },
-                params={"key": self.api_key},
-                **self.http_req_params,
-            ).json()
-            return "ok", response["candidates"][0]["content"]["parts"][0]["text"]
-        except Exception as e:
-            print(response)
-            # network error
-            import traceback
-
-            self.logger.warning(traceback.format_exc())
-            return "err", None
-
-    def query(self, seeds, queries):
-        # Gemini does not accept a seed option
-        # so we just ignore the seeds
-        self.logger.info("Querying LLM model...")
-        for query in queries:
-            self.logger.info(query + "\n")
-
-        responses = []
-        for query in queries:
-            for retry in range(self.num_retry):
-                status, response = self.restful_request(query)
-                if status == "ok":
-                    responses.append(response)
-                    break
-                else:
-                    if retry == self.num_retry - 1:
-                        self.logger.error(
-                            f"Failed to query Gemini for {self.num_retry} times. Abort!"
-                        )
-                        raise Exception("Failed to query Gemini")
-                    else:
-                        self.logger.warning(
-                            "Failed to query Gemini. Sleep and retry..."
-                        )
-                        time.sleep(30)
-
-        self.logger.info("LLM model responses:")
-        for response in responses:
-            self.logger.info(response + "\n")
-
-        return responses
-
-
 class TGIBackend:
     def __init__(
         self,
@@ -138,7 +61,6 @@ class TGIBackend:
                         UserMessage(content=query),
                     ],
                     max_tokens=self.max_tokens,
-                    seed=seed,
                     model=self.model,
                 )
                 content = response.choices[0].message.content
@@ -173,65 +95,3 @@ class TGIBackend:
             self.logger.info(content + "\n")
 
         return contents
-
-
-class HuggingfaceModel:
-    """Support huggingface models"""
-
-    def __init__(
-        self,
-        model,  # e.g. meta-llama/Meta-Llama-3-8B-Instruct
-        device_map="auto",
-        trust_remote_code=False,
-        num_workers=1,
-        **kwargs,
-    ):
-        import torch
-        import transformers
-        from transformers import AutoModelForCausalLM, AutoTokenizer, Conversation
-
-        self.logger = logging.getLogger("phylox")
-
-        self.tokenizer = AutoTokenizer.from_pretrained(
-            model, trust_remote_code=trust_remote_code
-        )
-        llm = torch.compile(
-            AutoModelForCausalLM.from_pretrained(
-                model, trust_remote_code=trust_remote_code, device_map=device_map
-            )
-        )
-        self.chatbot = transformers.pipeline(
-            "conversational",
-            model=llm,
-            device_map=device_map,
-            tokenizer=self.tokenizer,
-            trust_remote_code=trust_remote_code,
-            num_workers=num_workers,
-        )
-
-    def query(self, seeds, queries):
-        import torch
-
-        # torch doesn't support setting a batch of seeds for a batch of queries
-        # so we combine them into a single seed by XORing them
-        seed = reduce(lambda x, y: x ^ y, seeds)
-        torch.manual_seed(seed)
-        self.logger.info("Querying LLM model...")
-        for query in queries:
-            self.logger.info(query + "\n")
-
-        conversations = [Conversation(query) for query in queries]
-        conversations = self.chatbot(
-            conversations,
-            top_p=0.97,
-            pad_token_id=self.tokenizer.eos_token_id,
-            max_length=2048,
-        )
-        responses = [
-            conversation.messages[-1]["content"] for conversation in conversations
-        ]
-        self.logger.info("LLM model responses:")
-        for response in responses:
-            self.logger.info(response + "\n")
-
-        return responses
